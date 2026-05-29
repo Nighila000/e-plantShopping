80019285d7cf50a3a36e9873e06547f0f66e4dd0
Nighila000 <nighilasivan@gmail.com>
2026-05-29 00:26:23 -0400

    Implement cart reducers for adding, removing, and updating quantities

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,39 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const item = action.payload;
+
+   const existingItem = state.items.find(
+      (plant) => plant.name === item.name
+   );
+
+   if (existingItem) {
+      existingItem.quantity += 1;
+   } else {
+      state.items.push({
+         ...item,
+         quantity: 1,
+      });
+   }
     },
-    removeItem: (state, action) => {
+        removeItem: (state, action) => {
+   state.items = state.items.filter(
+      (item) => item.name !== action.payload
+   );
     },
     updateQuantity: (state, action) => {
+const { name, amount } = action.payload;
 
+   const item = state.items.find(
+      (item) => item.name === name
+   );
+
+   if (item) {
+      item.quantity = amount;
+   }
+},
     
-    },
+    
   },
 });
 
